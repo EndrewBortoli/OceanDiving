dc28676e4c044c98617294e6816c6d497f5f8069
Endrew Bortoli <124389686+EndrewBortoli@users.noreply.github.com>
2024-11-04 14:13:14 -0300

    Initial commit

---
+++ src/components/LpContent.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "../styles/SelectionComponent.css";
+import easyImage from '../assets/animal1.jpg';
+import mediumImage from '../assets/animal2.jpg';
+import hardImage from '../assets/animal3.jpg';
+import logo from '../assets/logooceanspace.png';
+
+const LpContent = ({ setDifficulty }) => {
+  const options = [
+    { label: "EASY", text: "Satellite Scout", image: easyImage },
+    { label: "MEDIUM", text: "Data Detective", image: mediumImage },
+    { label: "HARD", text: "Geo-Wizard", image: hardImage },
+  ];
+  const [currentIndex, setCurrentIndex] = useState(1); // Start at "MEDIUM"
+
+  const handleSelection = (index) => {
+    setCurrentIndex(index);
+    setDifficulty(options[index].label); // Update difficulty in the parent component
+  };
+
+  return (
+    <div className="selection-container">
+      <img src={logo} alt="Ocean Logo" className="logo" />
+      <h1>OCEANSPACE</h1>
+      <p className="text">
+        Welcome to the world of remote sensing, explorer! You have been selected to work on the <strong>OceansPACE</strong> program, where you will take on missions all over the world to generate reports and learn about <strong>PACE</strong> monitored phenomena and remote sensing.
+      </p>
+      <h6>Pick difficulty</h6>
+
+      <div className="image-selection">
+        {options.map((option, index) => (
+          <div
+            key={option.label}
+            className={`image-button ${index === currentIndex ? 'selected' : ''}`}
+            onClick={() => handleSelection(index)} // Modificado para permitir seleção direta
+          >
+            <img src={option.image} alt={`${option.label} icon`} />
+            <p>{option.text}</p>
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+};
+
+export default LpContent;
---
+++ src/pages/DifficultyPage.js
@@ -0,0 +1,123 @@
+import * as THREE from 'three';
+import React, { Suspense, useRef, useMemo, useState } from 'react';
+import { Canvas, extend, useThree, useLoader, useFrame } from '@react-three/fiber';
+import { Sky } from '@react-three/drei';
+import { Water } from 'three-stdlib';
+import '../styles/Difficulty.css';
+import LpContent from '../components/LpContent.js';
+import backgroundMusic from '../assets/sounds/background_water.mp3';
+import waterNormalsTexture from '../assets/waternormals.jpeg';
+import { useNavigate } from 'react-router-dom';
+
+extend({ Water });
+
+function Ocean({ speed }) {
+  const ref = useRef();
+  const gl = useThree((state) => state.gl);
+
+  // Load the water normals texture from the imported file
+  const waterNormals = useLoader(THREE.TextureLoader, waterNormalsTexture); 
+  waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
+
+  const geom = useMemo(() => new THREE.PlaneGeometry(10000, 10000), []);
+
+  const config = useMemo(
+    () => ({
+      textureWidth: 512,
+      textureHeight: 512,
+      waterNormals,
+      sunDirection: new THREE.Vector3(),
+      sunColor: 0xffffff,
+      waterColor: 0x003249,
+      distortionScale: 0,
+      fog: false,
+      format: gl.encoding,
+    }),
+    [waterNormals, gl.encoding]
+  );
+
+  useFrame((state, delta) => (ref.current.material.uniforms.time.value += delta * speed));
+
+  return <water ref={ref} args={[geom, config]} rotation-x={-Math.PI / 2} />;
+}
+
+export default function App() {
+  const [difficulty, setDifficulty] = useState('MEDIUM');
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(new Audio(backgroundMusic));
+  const navigate = useNavigate();
+
+  const toggleAudio = () => {
+    const audio = audioRef.current;
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.loop = true;
+      audio.play().catch((error) => console.log('Audio play failed:', error));
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const getSpeed = () => {
+    switch (difficulty) {
+      case 'EASY':
+        return 0.5;
+      case 'MEDIUM':
+        return 1;
+      case 'HARD':
+        return 2;
+      default:
+        return 1;
+    }
+  };
+
+  const setDifficultyAndSave = (level) => {
+    setDifficulty(level);
+    localStorage.setItem('selectedDifficulty', level);
+  };
+
+  const goToNextPage = () => {
+    navigate("/main");
+  };
+
+  return (
+    <>
+      <div className="canvas-container">
+        <Canvas camera={{ position: [0, 5, 100], fov: 55, near: 1, far: 20000 }}>
+          <pointLight position={[100, 100, 100]} />
+          <pointLight position={[-100, -100, -100]} />
+          <Suspense fallback={null}>
+            <Ocean speed={getSpeed()} />
+          </Suspense>
+          <Sky scale={1000} sunPosition={[0, 0, 50]} sunColor="yellow" turbidity={6} rayleigh={2} />
+        </Canvas>
+        <div className="black-overlay"></div>
+      </div>
+
+      <div className="lp-content-container">
+        <LpContent setDifficulty={setDifficultyAndSave} />
+      </div>
+
+      <button
+        onClick={toggleAudio}
+        style={{
+          position: 'absolute',
+          top: '20px',
+          left: '20px',
+          zIndex: 11,
+          padding: '10px 20px',
+          backgroundColor: isPlaying ? '#ECAA01' : '#1CAAD9',
+          color: 'white',
+          border: 'none',
+          borderRadius: '5px',
+          cursor: 'pointer'
+        }}
+      >
+        {isPlaying ? 'Mute' : 'Unmute'}
+      </button>
+      <button className="start-mission-button" onClick={goToNextPage}>
+        START MISSION
+      </button>
+    </>
+  );
+}
